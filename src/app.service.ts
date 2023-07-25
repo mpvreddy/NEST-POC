f045ef322a96da18c7ae3c5d9999547933a3e2f1
mpvreddy <130564594+mpvreddy@users.noreply.github.com>
2023-07-25 12:57:17 +0530

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'I love you PRAKASH \n Yours Lovingy VENKAT!';
+    return 'I love you PRAKASH';
+    return '\n Yours Lovingy VENKAT!';
   }
 }
