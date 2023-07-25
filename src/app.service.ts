982e7ccd335e49a218550c55e1c286bc321caa2e
mpvreddy <130564594+mpvreddy@users.noreply.github.com>
2023-07-25 13:09:45 +0530

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'I love you SRIKHA DA!';
+    return 'THE METALLIAN';
   }
 }
