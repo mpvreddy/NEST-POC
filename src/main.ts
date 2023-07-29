2becacc78b4160323da629827fb318cd8b8b0365
mpvreddy <130564594+mpvreddy@users.noreply.github.com>
2023-07-29 14:33:51 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -34,3 +34,41 @@ bootstrap();
 }
 
 bootstrap();
+
+
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as process from "process";
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT || 3000);
+}
+
+bootstrap();
+  await app.listen(process.env.PORT || 3000);
+}
+
+bootstrap();
+  await app.listen(process.env.PORT || 3000);
+}
+
+bootstrap();
+bootstrap();
+  await app.listen(process.env.PORT || 3000);
+}
+
+bootstrap();
+  await app.listen(process.env.PORT || 3000);
+}
+
+bootstrap();
+bootstrap();
+  await app.listen(process.env.PORT || 3000);
+}
+
+bootstrap();
+  await app.listen(process.env.PORT || 3000);
+}
+
+bootstrap();
